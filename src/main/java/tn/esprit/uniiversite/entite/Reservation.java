3eb93692dc51f4bfaba10b4c9e658c2a17bb8cbe
nidhal333 <nidhal.weslati@esprit.tn>
2023-10-10 09:26:08 +0100

    PUSH

--- src/main/java/tn/esprit/uniiversite/entite/Reservation.java
+++ src/main/java/tn/esprit/uniiversite/entite/Reservation.java
@@ -22,6 +22,6 @@ public class Reservation implements Serializable {
     private Boolean estValide;
 
     @ManyToMany(cascade = CascadeType.ALL)
-    private List<Etudiant> etudiants;
+    private Set<Etudiant> etudiants;
 
 }
